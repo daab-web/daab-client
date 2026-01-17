74816d203e008e17ac3198f0d3a7bb316b4a3380
Mahammad Farmanov <m.farm4nov@gmail.com>
2026-01-17 13:15:00 +0400

    feat: separator

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,3 +1,4 @@
+import { Separator } from "../ui/separator";
 import { BottomNavigationMenu } from "./bottom";
 import { TopNavigation } from "./top";
 
@@ -5,6 +6,7 @@ export default function Navbar() {
   return (
     <div className="flex flex-col gap-4">
       <TopNavigation />
+      <Separator />
       <BottomNavigationMenu />
     </div>
   );
