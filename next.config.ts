ea86f1f3ee505f69e4a102c6070bad9ec7f50ad3
Mahammad Farmanov <m.farm4nov@gmail.com>
2026-02-19 18:52:32 +0400

    refactor: throwing error if NEXT_PUBLIC_MINIO_HOST is not present

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,30 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
-import createMDX from "@next/mdx"
+import createMDX from "@next/mdx";
+
+const minioHost = process.env.NEXT_PUBLIC_MINIO_HOST;
+if (!minioHost) {
+  throw new Error("NEXT_PUBLIC_MINIO_HOST is not set");
+}
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.unsplash.com"
+        hostname: "**.unsplash.com",
       },
       {
-        hostname: "lh3.googleusercontent.com"
+        hostname: "lh3.googleusercontent.com",
       },
       {
         protocol: "http",
-        hostname: process.env.NEXT_PUBLIC_MINIO_HOST ?? "localhost"
-      }
-    ]
-  }
+        hostname: minioHost,
+      },
+    ],
+  },
 };
 
 const withMDX = createMDX({
