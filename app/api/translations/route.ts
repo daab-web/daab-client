e369adcba778a8f56f843e071f55e97e8985dad6
Mahammad Farmanov <m.farm4nov@gmail.com>
2026-03-07 16:17:54 +0400

    feat: fetching countries, areas and institutions translations from db

--- app/api/translations/route.ts
+++ app/api/translations/route.ts
@@ -1,16 +1,13 @@
-import DB from "@/db";
 import { NextRequest, NextResponse } from "next/server";
+import DB from "@/db"
 
 export async function GET(req: NextRequest) {
   const params = req.nextUrl.searchParams
   const locale = params.get("locale")
-  const namespace = params.get("namespace")
-
-  console.log(locale, namespace)
 
   DB.loadDatabase()
 
-  const data = await DB.findAsync({ locale, namespace });
+  const data = await DB.findOneAsync({ locale })
 
-  return NextResponse.json(data);
+  return NextResponse.json(data)
 }
