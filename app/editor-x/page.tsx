45256270b930e5f25981ebc442dd159523bf8eaa
Mahammad Farmanov <m.farm4nov@gmail.com>
2026-03-20 20:36:48 +0400

    fix: page gets scrolled to top when popping select

--- app/editor-x/page.tsx
+++ app/editor-x/page.tsx
@@ -1,17 +1,18 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-import { SerializedEditorState } from "lexical"
+import { useState } from "react";
+import { SerializedEditorState } from "lexical";
 
-import { Editor } from "@/components/blocks/editor-x/editor"
+import { Editor } from "@/components/blocks/editor-x/editor";
 
 export default function EditorPage() {
-  const [editorState, setEditorState] =
-    useState<SerializedEditorState>()
+  const [editorState, setEditorState] = useState<SerializedEditorState>();
   return (
-    <Editor
-      editorSerializedState={editorState}
-      onSerializedChange={(value) => setEditorState(value)}
-    />
-  )
+    <div className="rounded-lg border shadow w-225 h-185">
+      <Editor
+        editorSerializedState={editorState}
+        onSerializedChange={(value) => setEditorState(value)}
+      />
+    </div>
+  );
 }
