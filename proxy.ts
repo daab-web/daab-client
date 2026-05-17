38d673270103459dc89bd3deddc056a447811ece
Kenan <ibiyev0404@mail.ru>
2026-05-17 13:36:37 +0400

    feat : add hover tooltips to toolbar buttons

--- proxy.ts
+++ proxy.ts
@@ -9,9 +9,9 @@ export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   if (pathname.startsWith("/admin")) {
-    // if (process.env.ADMIN_DOMOY === "true") {
-    //   return NextResponse.next();
-    // }
+    if (process.env.ADMIN_DOMOY === "true") {
+      return NextResponse.next();
+    }
 
     const sessionCookie = getSessionCookie(request);
 
